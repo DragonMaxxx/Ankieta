9ece4395ba78b57b48915c3ed3d30431823a5260
DragonMaxxx <mb.mbartoszewicz@gmail.com>
2022-04-13 22:57:20 +0200

    Pierwsze zmiany

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+from flask import Flask
+app = Flask(__name__)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@app.route("/")
+def hello() -> str:
+    return "Hello World"
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    app.run(debug=False)
